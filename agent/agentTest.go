6cedc6e9f5a5a24db205bf841ba734e569737b74
anshujainb <jainb.anshu@gmail.com>
2018-11-01 01:38:53 -0700

    formatting spacing for agent

--- agent/agentTest.go
+++ agent/agentTest.go
@@ -2,63 +2,43 @@ package main
 
 import (
 	"context"
-
 	"encoding/json"
-
 	"fmt"
-
 	"io"
-
 	"log"
-
 	"os"
-
 	"strconv"
 
 	"github.com/docker/docker/api/types"
-	"github.com/docker/go-connections/nat"
-
 	"github.com/docker/docker/api/types/container"
-
 	"github.com/docker/docker/client"
+	nat "github.com/docker/go-connections/nat"
 )
 
 type ContainerType struct {
-	ConName string `json:"conName"`
-
+	ConName   string `json:"conName"`
 	ImageName string `json:"imageName"`
-
-	Port int `json:"port"`
-
-	ID string `json:"id"`
+	Port      int    `json:"port"`
+	ID        string `json:"id"`
 }
 
 func (c *ContainerType) RunContainer() {
 
 	ctx := context.Background()
-
 	cli, err := client.NewClientWithOpts(client.WithVersion("1.37"))
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	out, err := cli.ImagePull(ctx, c.ImageName, types.ImagePullOptions{})
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	io.Copy(os.Stdout, out)
 
 	portString := strconv.Itoa(c.Port) + "/tcp"
-
 	config := &container.Config{
-
 		Image: c.ImageName,
 		ExposedPorts: nat.PortSet{
 			nat.Port(portString): {},
@@ -66,63 +46,43 @@ func (c *ContainerType) RunContainer() {
 	}
 
 	resp, err := cli.ContainerCreate(ctx, config, nil, nil, c.ConName)
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	c.ID = resp.ID
 
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-
 		panic(err)
-
 	}
 
 	log.Println(resp.ID)
-
 }
 
 func (c *ContainerType) DestroyContainer() error {
-
 	return nil
-
 }
 
 func printContainer(container types.Container) {
-
 	res2B, _ := json.MarshalIndent(container, "", "\t")
-
 	fmt.Println(string(res2B))
-
 }
 
 func listContainer() {
 
 	cli, err := client.NewEnvClient()
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	for _, container := range containers {
-
 		fmt.Println(container.ID)
-
 		fmt.Println(container.Names[0])
-
 	}
 
 }
@@ -130,97 +90,49 @@ func listContainer() {
 func getContainer(container_name string) types.Container {
 
 	cli, err := client.NewEnvClient()
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
-	//containers, err := cli.ContainerList
-
-	//containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions.Filters  ({"name": [container_name]}))
-
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-
-	//io.Copy(os.Stdout, containers)
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	var containerTodel types.Container
-
 	for _, container := range containers {
-
 		if container.Names[0] == container_name {
-
 			containerTodel = container
-
 		}
-
-		//fmt.Println(container.name)
-
 	}
 
 	printContainer(containerTodel)
-
 	return containerTodel
-
 }
 
 func (c *ContainerType) stopContainer(conainerName string) {
 
 	conterToDel := getContainer(conainerName)
-
 	ctx := context.Background()
-
 	cli, err := client.NewEnvClient()
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
-	/*containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-
-	  if err != nil {
-
-	      panic(err)
-
-	  }*/
-
-	//for _, container := range conterToDel {
-
-	//container = types.Container(container)
-
 	fmt.Print("Stopping container ", conterToDel.ID, "... ")
-
 	if err := cli.ContainerStop(ctx, conterToDel.ID, nil); err != nil {
-
 		panic(err)
-
 	}
 
 	fmt.Println("Success")
-
-	//}
-
 }
 
 func printContainerLog(containerId string) {
 
 	ctx := context.Background()
-
 	cli, err := client.NewEnvClient()
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	options := types.ContainerLogsOptions{ShowStdout: true}
@@ -228,43 +140,27 @@ func printContainerLog(containerId string) {
 	// Replace this ID with a container that really exists
 
 	out, err := cli.ContainerLogs(ctx, containerId, options)
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	io.Copy(os.Stdout, out)
-
 }
 
 func ListAllImage() {
 
 	cli, err := client.NewEnvClient()
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	for _, image := range images {
-
-		//res, _ := json.MarshalIndent(image, "", "\t")
-
-		//fmt.Println(string(res))
-
 		fmt.Println(image.ID)
-
 	}
 
 }
@@ -272,25 +168,16 @@ func ListAllImage() {
 func pullImage(imageName string) {
 
 	ctx := context.Background()
-
 	cli, err := client.NewEnvClient()
-
 	if err != nil {
-
 		panic(err)
-
 	}
 
 	out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
-
 	if err != nil {
-
 		panic(err)
-
 	}
-
 	defer out.Close()
 
 	io.Copy(os.Stdout, out)
-
 }
--- agent/main.go
+++ agent/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"encoding/json"
-
 	"fmt"
-
 	"io/ioutil"
-
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -16,19 +13,13 @@ import (
 func main() {
 
 	r := mux.NewRouter()
-
 	r.HandleFunc("/api/runCon", action(runConHandler)).Methods("POST")
-
 	r.HandleFunc("/api/destroyCon", action(destroyConHandler)).Methods("POST")
 
 	n := negroni.New()
-
 	n.Use(negroni.NewRecovery())
-
 	n.Use(logger)
-
 	n.UseHandler(r)
-
 	n.Run(":8005")
 
 }
@@ -38,87 +29,55 @@ var logger = newLogger()
 type myHandlerFunc func(http.ResponseWriter, *http.Request) error
 
 func action(f myHandlerFunc) http.HandlerFunc {
-
 	return func(rw http.ResponseWriter, r *http.Request) {
-
 		if err := f(rw, r); err != nil {
-
 			fmt.Printf("[ERROR] %v\n", err)
-
 			http.Error(rw, err.Error(), http.StatusInternalServerError)
-
 			return
-
 		}
-
 	}
-
 }
 
 func newLogger() *negroni.Logger {
-
 	l := negroni.NewLogger()
-
 	return l
-
 }
 
 func runConHandler(rw http.ResponseWriter, r *http.Request) error {
-
 	body, err := ioutil.ReadAll(r.Body)
-
 	if err != nil {
-
 		fmt.Println(err)
-
 		return err
-
 	}
-
 	defer r.Body.Close()
 
 	var con ContainerType
-
 	if err := json.Unmarshal(body, &con); err != nil {
-
 		fmt.Println(err)
-
 		return err
-
 	}
 
 	con.RunContainer()
 
 	return nil
-
 }
 
 func destroyConHandler(rw http.ResponseWriter, r *http.Request) error {
 
 	body, err := ioutil.ReadAll(r.Body)
-
 	if err != nil {
-
 		fmt.Println(err)
-
 		return err
-
 	}
-
 	defer r.Body.Close()
 
 	var con ContainerType
-
 	if err := json.Unmarshal(body, &con); err != nil {
-
 		fmt.Println(err)
-
 		return err
-
 	}
 
 	con.DestroyContainer()
 
 	return nil
-
 }
